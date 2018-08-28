7b7b3bd978e754edc291169faae9589a8c5f3396
wley3337 <wley3337@gmail.com>
2018-08-28 10:35:18 -0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :notes
-  resources :users
+  post '/users/login', to: 'users#login'
+  get '/users/my-page', to: 'users#show'
+  patch '/users/my-page', to: 'users#update'
+  resources :notes, only: [:index]
+  resources :users, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
