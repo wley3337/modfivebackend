c5485e34d4639684022c1bea083fa2b6ab7a7878
wley3337 <wley3337@gmail.com>
2018-08-28 14:05:16 -0400

    added presence of first and last name validation

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,7 @@ class Note < ApplicationRecord
     belongs_to :user
     has_many :note_catagories
     has_many :catagories, through: :note_catagories
+    accepts_nested_attributes_for :catagories
     
     # limits the kind of notes catagory associations return with json
     def self.public?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :notes
     accepts_nested_attributes_for :notes
-    validates :username, uniqueness: true 
+    validates :username, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true  
     has_secure_password
 end
