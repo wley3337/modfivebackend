2f47496d08af39c430e22947f13a717f8648ccc6
wley3337 <wley3337@gmail.com>
2018-08-28 10:32:24 -0400

    initial generation

---
+++ db/migrate/20180828141900_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.string :roll
+      t.date :start_date
+      
+
+      t.timestamps
+    end
+  end
+end
