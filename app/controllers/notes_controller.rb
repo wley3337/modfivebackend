a46985104c144f79c5a55dc3d7bf5c49ff93ffb0
wley3337 <wley3337@gmail.com>
2018-09-01 11:43:54 -0400

    can delete note from front double confirmation that owner is owner, changed eager_load to true for all env and seemed to fix lockup crash

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,16 @@ class NotesController < ApplicationController
          render json:  Note.public?.map{|note| note.serialize_note}
     end
 
-    def update
+    def destroy
+        #local strong params
+        note_params = params.require(:note).permit(:id)
+        user = current_user
+        note = Note.find(note_params["id"])
+        if current_user.id === note.user_id
+            
+            note.delete
+        end 
+        render json: current_user.serialize_user
     end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/users/login', to: 'users#login'
   get '/users/my-page', to: 'users#show'
   patch '/users/my-page', to: 'users#update'
-  
+  delete '/notes', to: 'notes#destroy'
   resources :notes, only: [:index]
   resources :users, only: [:create]
   resources :categories, only: [:index]
