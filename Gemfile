ced20abe89a8bab75189aed20c2cfdac42c1ecde
wley3337 <wley3337@gmail.com>
2018-09-12 08:39:40 -0400

    large seed data file added

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
-
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 ruby '2.3.7'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,70 +15,163 @@ user1 = User.create(username: "user1", first_name: "user1FristName", last_name: 
 
 user2 = User.create(username: "user2", first_name: "user2FristName", last_name: "user2LastName", password: "user2123", start_date: "2012-01-01".to_date, roll: "student")
 
-
-c1 = Category.create(name: "Mod 1")
-c2 = Category.create(name: "Mod 2")
-c3 = Category.create(name: "Mod 3")
-c4 = Category.create(name: "Mod 4")
-c5 = Category.create(name: "Mod 5")
-c6 = Category.create(name: "JavaScript")
-c7 = Category.create(name: "Ruby")
-c8 = Category.create(name: "React")
-
-n1 = Note.create(note_content: "Lose eyes get fat shew. Winter can indeed letter oppose way change tended now. So is improve my charmed picture exposed adapted demands. Received had end produced prepared diverted strictly off man branched. Known ye money so large decay voice there to. Preserved be mr cordially incommode as an. He doors quick child an point at. Had share vexed front least style off why him. 
-
-Did shy say mention enabled through elderly improve. As at so believe account evening behaved hearted is. House is tiled we aware. It ye greatest removing concerns an overcame appetite. Manner result square father boy behind its his. Their above spoke match ye mr right oh as first. Be my depending to believing perfectly concealed household. Point could to built no hours smile sense. ", public_note: true, user_id: 2)
-
-n2 = Note.create(note_content: "His exquisite sincerity education shameless ten earnestly breakfast add. So we me unknown as improve hastily sitting forming. Especially favourable compliment but thoroughly unreserved saw she themselves. Sufficient impossible him may ten insensible put continuing. Oppose exeter income simple few joy cousin but twenty. Scale began quiet up short wrong in in. Sportsmen shy forfeited engrossed may can. 
-
-No depending be convinced in unfeeling he. Excellence she unaffected and too sentiments her. Rooms he doors there ye aware in by shall. Education remainder in so cordially. His remainder and own dejection daughters sportsmen. Is easy took he shed to kind. ", public_note: true, user_id: 2)
-
-n3 = Note.create(note_content: "An country demesne message it. Bachelor domestic extended doubtful as concerns at. Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel. Depending conveying direction has led immediate. Law gate her well bed life feet seen rent. On nature or no except it sussex. 
-
-Why painful the sixteen how minuter looking nor. Subject but why ten earnest husband imagine sixteen brandon. Are unpleasing occasional celebrated motionless unaffected conviction out. Evil make to no five they. Stuff at avoid of sense small fully it whose an. Ten scarcely distance moreover handsome age although. As when have find fine or said no mile. He in dispatched in imprudence dissimilar be possession unreserved insensible. She evil face fine calm have now. Separate screened he outweigh of distance landlord.  ", public_note: false, user_id: 2)
-
-n4 = Note.create(note_content: "An country demesne message it. Bachelor domestic extended doubtful as concerns at. Morning prudent removal an letters by. On could my in order never it. Or excited certain sixteen it to parties colonel. Depending conveying direction has led immediate. Law gate her well bed life feet seen rent. On nature or no except it sussex. 
-
-Why painful the sixteen how minuter looking nor. Subject but why ten earnest husband imagine sixteen brandon. Are unpleasing occasional celebrated motionless unaffected conviction out. Evil make to no five they. Stuff at avoid of sense small fully it whose an. Ten scarcely distance moreover handsome age although. As when have find fine or said no mile. He in dispatched in imprudence dissimilar be possession unreserved insensible. She evil face fine calm have now. Separate screened he outweigh of distance landlord.  ", public_note: false, user_id: 3)
-
-n5 = Note.create(note_content: "Maids table how learn drift but purse stand yet set. Music me house could among oh as their. Piqued our sister shy nature almost his wicket. Hand dear so we hour to. He we be hastily offence effects he service. Sympathize it projection ye insipidity celebrated my pianoforte indulgence. Point his truth put style. Elegance exercise as laughing proposal mistaken if. We up precaution an it solicitude acceptance invitation. 
-
-Travelling alteration impression six all uncommonly. Chamber hearing inhabit joy highest private ask him our believe. Up nature valley do warmly. Entered of cordial do on no hearted. Yet agreed whence and unable limits. Use off him gay abilities concluded immediate allowance.", public_note: false, user_id: 3)
-
-n1.categories << c3
-n1.categories << c6
-n2.categories << c1
-n2.categories << c7
-n4.categories << c4
-n4.categories << c6
-n5.categories << c6
+user3 = User.create(username: "user3", first_name: "user3FristName", last_name: "user3LastName", password: "user3123", start_date: "2012-01-01".to_date, roll: "student")
 
 
-user2.notes << n1
-user2.notes << n2
-user2.notes << n3
+# category seeds
 
-user1.notes << n5
-user1.notes << n4
+    c1 = Category.create(name: "Mod 1")
+    c2 = Category.create(name: "Mod 2")
+    c3 = Category.create(name: "Mod 3")
+    c4 = Category.create(name: "Mod 4")
+    c5 = Category.create(name: "Mod 5")
+    c6 = Category.create(name: "JavaScript")
+    c7 = Category.create(name: "Ruby")
+    c8 = Category.create(name: "React")
+    c9 = Category.create(name: "Redux")
+    c10 = Category.create(name: "CSS")
+    c11 = Category.create(name: "Rails")
+    c12 = Category.create(name: "Redux")
+
+
+catArray = [c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12]
+x=0
+while x <= 310 
+
+    n = Note.create(
+        note_content: Faker::Lovecraft.paragraph(rand(1..8)) + Faker::Lovecraft.paragraph(rand(1..8)),
+        public_note: true,
+        user_id: 2
+    )
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    user1.notes << n
+    x += 1
+end
+x=0
+while x <= 310 
+
+    n = Note.create(
+        note_content: Faker::Lovecraft.paragraph(rand(1..8)) + Faker::Lovecraft.paragraph(rand(1..8)),
+        public_note: true,
+        user_id: 3
+    )
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    user2.notes << n
+    x += 1
+end
+x=0
+while x <= 310 
+
+    n = Note.create(
+        note_content: Faker::Lovecraft.paragraph(rand(1..8)) + Faker::Lovecraft.paragraph(rand(1..8)),
+        public_note: true,
+        user_id: 4
+    )
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    user3.notes << n
+    x += 1
+end
+
+x=0
+while x <= 50 
+
+    n = Note.create(
+        note_content: Faker::Lovecraft.paragraphs(10),
+        public_note: false,
+        user_id: 3
+    )
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    user2.notes << n
+    x += 1
+end
+x=0
+while x <= 50 
+
+    n = Note.create(
+        note_content: Faker::Lovecraft.paragraph(10),
+        public_note: false,
+        user_id: 4
+    )
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    n.categories << catArray[rand(0..11)]
+    user3.notes << n
+    x += 1
+end
+
+
+
+# reference and category assignment
 
 r1 = Reference.create(title: "React Router Docs", link: "https://reacttraining.com/react-router/web/guides/quick-start", saves: 1)
-
-r2 = Reference.create(title: "Redux", link: "https://redux.js.org/", saves: 1)
-
-r3 = Reference.create(title: "Ruby Array Methods", link: "https://docs.ruby-lang.org/en/2.0.0/Array.html")
-r4 = Reference.create(title: "Ruby Hash Methods", link: "https://docs.ruby-lang.org/en/2.0.0/Hash.html")
-r5 = Reference.create(title: "Ruby String Methods", link: "https://ruby-doc.org/core-2.2.0/String.html", saves: 1)
-
 r1.categories << c3
 r1.categories << c8
+
+r2 = Reference.create(title: "Redux", link: "https://redux.js.org/", saves: 1)
 r2.categories << c8
 r2.categories << c4
 
+r3 = Reference.create(title: "Ruby Array Methods", link: "https://docs.ruby-lang.org/en/2.0.0/Array.html")
 r3.categories << c7
+
+r4 = Reference.create(title: "Ruby Hash Methods", link: "https://docs.ruby-lang.org/en/2.0.0/Hash.html")
 r4.categories << c7
+
+r5 = Reference.create(title: "Ruby String Methods", link: "https://ruby-doc.org/core-2.2.0/String.html", saves: 1)
 r5.categories << c7
 
+
+r6 = Reference.create(title: "Lucid Chart", link: "https://www.lucidchart.com/users/login#docs?folder_id=home&browser=icon&sort=saved-desc")
+r6.categories << c5
+
+r7 = Reference.create(title: "Color Palettes", link: "https://coolors.co/")
+r7.categories << c10
+
+r8 = Reference.create(title: "React Docs", link: "https://reactjs.org/")
+r8.categories << c5
+r8.categories << c8
+
+r9 = Reference.create(title: "Semantic UI React", link: "https://react.semantic-ui.com/")
+r9.categories << c8
+r9.categories << c4
+r9.categories << c5
+
+r10 = Reference.create(title: "Rails Docs", link: "https://edgeguides.rubyonrails.org/")
+r10.categories << c2
+r10.categories << c7
+r10.categories << c11
+
+r11 = Reference.create(title: "Google Fonts", link: "https://fonts.google.com/")
+r11.categories << c10
+r11.categories << c2 
+r11.categories << c3 
+r11.categories << c4
+r11.categories << c5
+
+
+
+# user reference assignments
 user1.references << r1 
 user1.references << r2
-user1.references << r5
+user1.references << r4
 
+
+user2.references << r2 
+user2.references << r10
+user2.references << r11 
+user2.references << r6 
+user2.references << r8
+
+user3.references << r3 
+user3.references << r2
+user3.references << r1
+user3.references << r7 
+user3.references << r10
